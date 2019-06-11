afb5a724f1511a4cc533b7aaa2b4e003d132f563
pandav <virjanand@virjanand.com>
2019-06-11 16:15:23 +0200

    Remove duplication of --- between test and implementation

--- src/main/java/com/virjanand/BowlingGame.java
+++ src/main/java/com/virjanand/BowlingGame.java
@@ -4,11 +4,9 @@ import java.util.Collection;
 import java.util.stream.IntStream;
 
 class BowlingGame {
-    private String scorecard;
     IntStream scorestream;
 
     public BowlingGame(String scorecard) {
-        this.scorecard = scorecard;
         scorestream = scorecard.chars();
     }
 
@@ -16,9 +14,6 @@ class BowlingGame {
         if (scorestream.filter(score -> score == '1').count() > 0) {
             return 1;
         }
-        if (scorecard.equals("--------------------")) {
-            return 0;
-        }
-        return -1;
+        return 0;
     }
 }
