f8ff675fae19724a01984202428c3124a59a407e
Virjanand <virjanand@virjanand.com>
2019-06-15 22:56:40 +0200

    change idea: cannot use stream, so use ListArray

--- src/main/java/com/virjanand/BowlingGame.java
+++ src/main/java/com/virjanand/BowlingGame.java
@@ -1,33 +1,42 @@
 package com.virjanand;
 
-class BowlingGame {
-    public static final char ZERO = '0';
-    public static final int MAX_SCORE = 10;
-    char[] scores;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public BowlingGame(String scorecard) {
-        scores = scorecard.toCharArray();
+class BowlingGame {
+    private static final char ZERO = '0';
+    private static final int MAX_SCORE = 10;
+    List<Character> scoresList;
+
+    BowlingGame(String scorecard) {
+        scoresList = scorecard.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
     }
 
     int getScore() {
         int sum = 0;
-        for (char score : scores) {
-            if (isNonZero(score)) sum = sumScores(sum, convertScore(score));
+        replaceZeros();
+        List<Integer> scoresAsInt = convertToNumbers();
+        for (int score : scoresAsInt) {
+            sum += score;
         }
         return sum;
     }
 
-    private boolean isNonZero(int score) {
-        return score != '-';
+    private List<Integer> convertToNumbers() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < scoresList.size(); i++) {
+            if (scoresList.get(i) == 'X')
+                result.add(MAX_SCORE + (scoresList.get(i + 1) - ZERO));
+            else result.add(scoresList.get(i) - ZERO);
+        }
+        return result;
     }
 
-    private int sumScores(int sum, int nextThrow) {
-        if (sum == 'X' - ZERO)
-            return MAX_SCORE + 2 * nextThrow;
-        return sum + nextThrow;
-    }
-
-    private int convertScore(int score) {
-        return score - ZERO;
+    private void replaceZeros() {
+        for (int i = 0; i < scoresList.size(); i++) {
+            if (scoresList.get(i) == '-')
+                scoresList.set(i, ZERO);
+        }
     }
 }
